05129c6aaa548fa3255ba7e1a82032e7a5be5093
Livya Gomes <livyagomesdc@gmail.com>
2024-10-11 15:33:15 -0300

    Alert

--- AndroidSeeker/AppHeader/AppHeaderRefreshButtonView.swift
+++ AndroidSeeker/AppHeader/AppHeaderRefreshButtonView.swift
@@ -11,29 +11,45 @@ import SwiftUI
 struct AppHeaderRefreshButtonView: View {
     @State private var rotationAngle: Double = 0
     @EnvironmentObject var deviceManager: DeviceManager
+    @State private var showAlert = false
     
     var body: some View {
-            HStack {
-                Button(action: {
-                    deviceManager.isLoading = true
-                    deviceManager.runADBDevices()
-                    
-                    print(deviceManager.devices)
-                    withAnimation(.linear(duration: 1.0)) {
-                        rotationAngle += 360
-                    }
-                }) {
-                    Image(systemName: "arrow.clockwise")
-                        .font(.title3)
-                        .foregroundColor(.blue)
-                        .padding(20)
-                        .rotationEffect(.degrees(rotationAngle))
+        HStack {
+            Button(action: {
+                Task {
+                    await deviceManager.runADBDevices()
+                    devicesCheck()
                 }
-                .background(Color.white)
-                .clipShape(Circle())
+                print(deviceManager.devices)
+                
+            
+                withAnimation(.linear(duration: 1.0)) {
+                    rotationAngle += 360
+                }
+            }) {
+                Image(systemName: "arrow.clockwise")
+//                Text("Listar dispositivos")
+                    .font(.title3)
+                    .foregroundColor(.blue)
+                    .padding(20)
+                    .cornerRadius(5.0)
+                    .rotationEffect(.degrees(rotationAngle))
             }
+            .background(.white)
+            .clipShape(Circle())
+            
+        }.alert(isPresented: $showAlert){
+            Alert(title: Text("Android Seeker"), message: Text("Nenhum dispositivo encontrado."), dismissButton: .default(Text("Ok")))
+        }
         
-        
+    }
+    
+    func devicesCheck() {
+        DispatchQueue.global(qos: .background).async {
+            if deviceManager.devices.isEmpty {
+                showAlert = true
+            }
+        }
     }
 }
 
