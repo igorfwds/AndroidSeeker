bbb7088fe15b23cfbd2d0adb519110925d1e592e
Igor Wanderley <igorfwds@hotmail.com>
2024-10-11 15:36:12 -0300

    loading view

--- AndroidSeeker/AppHeader/AppHeaderRefreshButtonView.swift
+++ AndroidSeeker/AppHeader/AppHeaderRefreshButtonView.swift
@@ -14,6 +14,21 @@ struct AppHeaderRefreshButtonView: View {
     @State private var showAlert = false
     
     var body: some View {
+            HStack {
+                Button(action: {
+                    deviceManager.isLoading = true
+                    deviceManager.runADBDevices()
+                    
+                    print(deviceManager.devices)
+                    withAnimation(.linear(duration: 1.0)) {
+                        rotationAngle += 360
+                    }
+                }) {
+                    Image(systemName: "arrow.clockwise")
+                        .font(.title3)
+                        .foregroundColor(.blue)
+                        .padding(20)
+                        .rotationEffect(.degrees(rotationAngle))
         HStack {
             Button(action: {
                 Task {
@@ -26,14 +41,8 @@ struct AppHeaderRefreshButtonView: View {
                 withAnimation(.linear(duration: 1.0)) {
                     rotationAngle += 360
                 }
-            }) {
-                Image(systemName: "arrow.clockwise")
-//                Text("Listar dispositivos")
-                    .font(.title3)
-                    .foregroundColor(.blue)
-                    .padding(20)
-                    .cornerRadius(5.0)
-                    .rotationEffect(.degrees(rotationAngle))
+                .background(Color.white)
+                .clipShape(Circle())
             }
             .background(.white)
             .clipShape(Circle())
