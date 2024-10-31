c1530f58aba770baa9ed9eb47b5c35b16a561130
Igor Wanderley <igorfwds@hotmail.com>
2024-10-31 16:31:36 -0300

    Starting to migrate to XPC service

--- AndroidSeeker/Model/DeviceManager.swift
+++ AndroidSeeker/Model/DeviceManager.swift
@@ -10,53 +10,35 @@ import Combine
 
 class DeviceManager: ObservableObject {
     
-    @Published var devices: [Device] = []
-    @Published var isLoading = false
+    private var connectionToService: NSXPCConnection!
+    
+    private func conectar() {
+        self.connectionToService = NSXPCConnection(serviceName: "igor.cesar.learning.DeviceManagerService")
+        self.connectionToService.remoteObjectInterface = NSXPCInterface(with: DeviceManagerServiceProtocol.self)
+        self.connectionToService.exportedObject = self
+        self.connectionToService.exportedInterface = NSXPCInterface(with: DeviceManagerServiceProtocol.self)
+        self.connectionToService.interruptionHandler = {
+          NSLog("Conexão interrompida")
+          self.connectionToService = nil
+        }
+        self.connectionToService.invalidationHandler = {
+          NSLog("Conexão invalidada")
+          self.connectionToService = nil
+        }
+        self.connectionToService.resume()
+      }
+
+    public func XPCservice() -> DeviceManagerServiceProtocol {
+        if self.connectionToService == nil {
+            self.conectar()
+        }
+        return self.connectionToService.remoteObjectProxyWithErrorHandler { (error) in
+            NSLog("Erro de conexão ao recuperar serviço: ", error.localizedDescription)
+        } as! DeviceManagerServiceProtocol
+    }
     
     func runADBDevices() async {
-        
-        guard let url = Bundle.main.url(forResource: "adb", withExtension: nil) else { return }
-        
-        let task = Process()
-        task.executableURL = url
-        task.arguments = ["devices"]
-        
-        let outputPipe = Pipe()
-        let errorPipe = Pipe()
-        
-        task.standardOutput = outputPipe
-        task.standardError = errorPipe
-        
-        do {
-            try task.run()
-            task.waitUntilExit()
-            
-            let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
-            let output = String(data: outputData, encoding: .utf8) ?? ""
-            
-            let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
-            let errorOutput = String(data: errorData, encoding: .utf8) ?? ""
-            
-            DispatchQueue.main.async {
-                if !output.isEmpty {
-                    let lines = output.split(separator: "\n").map(String.init)
-                    self.devices = lines.dropFirst().compactMap { line in
-                        let components = line.split(separator: "\t").map(String.init)
-                        guard components.count > 1 else { return nil }
-                        return Device(name: components[0], status: components[1], files: [])
-                    }
-                    self.isLoading = false
-                }
-            }
-            
-            if !errorOutput.isEmpty {
-                print("Erros do comando:\n\(errorOutput)")
-            }
-            
-        } catch {
-            print("Erro ao rodar adb: \(error)")
-        }
-        
+        XPCservice().runADBDevices()
     }
     
     
@@ -427,7 +409,7 @@ class DeviceManager: ObservableObject {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss" // Define o formato de entrada da string
         dateFormatter.locale = Locale(identifier: "en_US_POSIX") // Garante a consistência do formato
-        print("\nData formatada => \(dateFormatter.date(from: dateString))")
+        print("\nData formatada => \(String(describing: dateFormatter.date(from: dateString)))") //se der problema na data foi o describing
         print("Data recebida => \(dateString)")
         return dateFormatter.date(from: dateString)
     }
